65e55bd251ec5c1cc55c16c778ed0dcb67d5b072
cairow <cairowilker@gmail.com>
2023-10-18 14:43:19 -0400

    Login-Model-Controller-Views

---
+++ Helper/ISessao.cs
@@ -0,0 +1,11 @@
+﻿using Fut360.Models;
+
+namespace Fut360.Helper
+{
+    public interface ISessao
+    {
+        void CriarSessaoDoUsuario(UsuarioModel usuario);
+        void RemoverSessaoDoUsuario(UsuarioModel usuario);
+        UsuarioModel BuscarSessaoDoUsuario();
+    }
+}
